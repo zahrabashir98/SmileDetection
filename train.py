6ebef05de12e529e0ec06433bc7d8445b4010895
zahrabashir98 <zahra_9877@yahoo.com>
2019-01-25 16:21:20 +0330

    Add trainer

---
+++ train.py
@@ -0,0 +1,99 @@
+from __future__ import print_function
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+import numpy as np
+
+
+batch_size = 128
+num_classes = 2
+epochs = 50
+
+# input image dimensions
+img_rows, img_cols = 48, 48
+
+# the data, shuffled and split between train and test sets
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+
+with open('./data/fer2013.csv') as file:
+    lines = file.read().split('\n')[1:]
+    lines.remove('')
+
+    for line in lines:
+        emotion, pixels, usage = line.split(',')
+        if not (emotion == '0' or emotion == '1'):
+            continue
+
+        if usage == 'Training':
+            x, y = x_train, y_train
+        elif usage == 'PrivateTest':
+            x, y = x_test, y_test
+        else:
+            continue
+
+        pixels = [int(p) for p in pixels.split(' ')]
+        x.append(np.array(pixels))
+        y.append(int(emotion))
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=(48, 48, 1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+# for w in model.get_weights():
+#     print(w.shape)
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          )
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
